d1238ac4649e08cab07574b52a4adfa5c01d38fc
gazimovarsen <gazimovarsen@gmail.com>
2024-10-05 19:37:36 +0500

    Добавьте файлы проекта.

---
+++ SRV_2/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SRV_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtBuffer = new System.Windows.Forms.TextBox();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtBuffer
+            // 
+            this.txtBuffer.Location = new System.Drawing.Point(12, 288);
+            this.txtBuffer.Multiline = true;
+            this.txtBuffer.Name = "txtBuffer";
+            this.txtBuffer.Size = new System.Drawing.Size(385, 150);
+            this.txtBuffer.TabIndex = 1;
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(403, 288);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(385, 150);
+            this.txtResults.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.txtBuffer);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtBuffer;
+        private System.Windows.Forms.TextBox txtResults;
+    }
+}
+
---
+++ SRV_2/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SRV_2
+{
+    public partial class Form1 : Form
+    {
+        private const int bufferSize = 10;
+        private const int numberOfThreads = 4;
+        Queue<char> buffer1 = new Queue<char>();
+        Queue<char> buffer2 = new Queue<char>();
+        Random random = new Random();
+        SemaphoreSlim semaphore = new SemaphoreSlim(0, bufferSize);
+        bool running;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!running)
+            {
+                btnStart.Text = "Stop";
+                running = true;
+
+                ThreadPool.QueueUserWorkItem(Producer);
+                for(int i = 0; i < numberOfThreads; i++)
+                {
+                    ThreadPool.QueueUserWorkItem(Consumer, i);
+                }
+            }
+            else
+            {
+                btnStart.Text = "Start";
+                Kill();
+            }
+        }
+
+        void Producer(object state)
+        {
+            while (running)
+            {
+                try
+                {
+                    char message = (char)('A' + random.Next(26));
+
+                    if (buffer1.Count >= bufferSize)
+                    {
+                        buffer1.Dequeue();
+                    }
+
+                    buffer1.Enqueue(message);
+                    UpdateBufferDisplay(buffer1, txtBuffer);
+                    if (semaphore.CurrentCount < bufferSize)
+                    {
+                        semaphore.Release();
+                    }
+
+                    Thread.Sleep(1000);
+                }
+                catch (Exception e)
+                {
+                    NewResult(e.Message);
+                }
+            }
+        }
+
+        void Consumer(object state)
+        {
+            while (running)
+            {
+                try
+                {
+                    semaphore.Wait();
+
+                    if (buffer1.Count > 0)
+                    {
+                        char message = buffer1.Dequeue();
+                        buffer2.Enqueue(message);
+
+                        UpdateResultsDisplay(message);
+                        NewResult("Потоком " + Thread.CurrentThread.ManagedThreadId);
+                    }
+
+                    Thread.Sleep(5000);
+                }
+                catch (Exception e)
+                {
+                    NewResult(e.Message);
+                }
+            }
+        }
+
+        void UpdateBufferDisplay(Queue<char> buffer, TextBox textBox)
+        {
+            if (textBox.InvokeRequired)
+            {
+                textBox.Invoke(new Action(() => UpdateBufferDisplay(buffer, textBox)));
+            }
+            else
+            {
+                textBox.Text = string.Join(Environment.NewLine, buffer.ToArray());
+            }
+        }
+
+        void UpdateResultsDisplay(char message)
+        {
+            if (txtResults.InvokeRequired)
+            {
+                txtResults.Invoke(new Action(() => UpdateResultsDisplay(message)));
+            }
+            else
+            {
+                txtResults.AppendText($"Обработан {message}{Environment.NewLine}");
+            }
+        }
+
+        void NewResult(string message)
+        {
+            txtResults.AppendText($"{message}{Environment.NewLine}{Environment.NewLine}");
+        }
+
+
+        private void Kill()
+        {
+            running = false;
+            semaphore.Dispose();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Kill();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Kill();
+        }
+    }
+}
